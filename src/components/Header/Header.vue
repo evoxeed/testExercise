1147f1240b71921376a544f6ac9b2db9426f3a36
Nikitarg1 <nikita.gomozov@gmail.com>
2021-02-25 17:16:59 +0300

    init

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-header">
+    <Navbar></Navbar>
+    <b-container class="text-center text-white splash">
+      <h2 class="headingTwoCustom">We Are Awesome Creative Agency</h2>
+      <hr>
+      <p class="px-lg-5">This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
+        lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet
+        nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.</p>
+      <button class="text-uppercase mt-5">learn more</button>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Header/Navbar";
+
+export default {
+  name: 'Header',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-navbar toggleable="lg" variant="faded" type="dark">
+    <b-container>
+      <b-navbar-brand href="#">
+        <img :src="require('@/assets/img/logo.png')" height="60px" class="d-inline-block align-bottom" alt="tajam logo">
+        <span class="logoCustom align-bottom">tajam</span>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto text-uppercase navbarCustom">
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">about</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">expertise</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">teams</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">works</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">people say</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="navbarCustomItem">contact</a>
+          </li>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+</style>
